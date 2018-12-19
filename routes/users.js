c88671c2d0f089a1623b3ae0ec0269f70a7f6d59
li guodong <624117671@qq.com>
2018-12-19 18:28:30 +0800

    更新

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../db/db');
+var userSql = require("../sql/user");
+
+
+
+// 向前台返回JSON方法的简单封装
+var jsonWrite = function (res, ret) {
+	if(typeof ret === 'undefined') {
+		res.json({
+			code:'1',
+			msg: '操作失败'
+		});
+	} else {
+		res.json(ret);
+	}
+};
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -13,6 +28,20 @@ router.get('/list', function(req, res, next) {
 		let rows = JSON.stringify(vals);
 		res.send(rows)
 	})
+});
+
+router.get('/login', function(req, res, next) {
+  console.log(res)
+  var param = req.query || req.params;
+  db.getConnection(function(err,conn){
+    if (err) throw err;
+    conn.query(userSql.login,[param.account,param.password],function(err,result){
+        // 以json形式，把操作结果返回给前台页面
+        jsonWrite(res, result);
+        // 释放连接 
+        conn.release();
+    })
+})
 });
 
 module.exports = router;
--- sql/user.js
+++ sql/user.js
@@ -1,3 +1,6 @@
 var user = {
-    login:"select * from "
+    login:"select * from users where account = ? and password = ?",
+    insert:"insert into users (id,account,password,type,createTime,updateTime,staut) values(?,?,?,?,?,?,?)",
+    
 }
+module.exports=user;
